c270df49997802773a84784ca2e735d0fca5e6bc
thoratica <me@tica.fun>
2022-08-27 18:30:52 +0900

    entryservice.ts 삭제

--- example.ts
+++ example.ts
@@ -1,8 +1,5 @@
-import { EntryService, setLogon, User } from "./mod.ts";
+import { setLogon, User } from "./mod.ts";
 
 const dukhwa = new User({ id: "60bc5559659bf40bd15d022c" });
 
-
 // console.log(await dukhwa.toJSON());
-
-EntryService.getUserIdByUsername("dukhwa");
--- mod.ts
+++ mod.ts
@@ -3,6 +3,5 @@ import createVar from "./src/var.ts";
 const [getLogon, setLogon] = createVar(false);
 
 export { getLogon, setLogon };
-export { default as EntryService } from "./src/classes/entry.ts";
 export { default as User } from "./src/classes/user.ts";
 export { getCSRFToken } from "./src/utils.ts";
